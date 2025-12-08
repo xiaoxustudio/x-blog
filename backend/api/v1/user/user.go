1da0dd6ef7424b97acc53136cabbfb23b7627605
xiaoxustudio <1783558957@qq.com>
2025-12-08 14:59:19 +0800

    chore(backend): adjust the register verify info

--- backend/api/v1/user/user.go
+++ backend/api/v1/user/user.go
@@ -6,7 +6,7 @@ type (
 	RegisterReq struct {
 		g.Meta   `path:"/user/register" method:"POST" summary:"用户注册"`
 		Username string `json:"username" v:"required|length:3,50#用户名不能为空|用户名长度为3到50位"`
-		Password string `json:"password" v:"required|length:6,30#密码不能为空|密码长度为6到30位"`
+		Password string `json:"password" v:"required|length:6,50#密码不能为空|密码长度为6到50位"`
 		Email    string `json:"email" v:"required|email#邮箱不能为空|邮箱格式不正确"`
 	}
 	RegisterRes struct{}
