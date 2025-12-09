3e80ec4c2752d3681e73cff896174f7720fbf768
xiaoxustudio <1783558957@qq.com>
2025-12-09 20:18:02 +0800

    chore(build): vite build add chunk split

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,65 @@ export default defineConfig({
 				target: "http://localhost:8000"
 			}
 		}
+	},
+	build: {
+		chunkSizeWarningLimit: 1500,
+
+		emptyOutDir: true,
+
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id
+							.toString()
+							.split("node_modules/.pnpm/")[1]
+							.split("/")[0]
+							.toString();
+					}
+				},
+
+				chunkFileNames: (chunkInfo) => {
+					const facadeModuleId = chunkInfo.facadeModuleId
+						? chunkInfo.facadeModuleId.split("/")
+						: [];
+
+					const fileName =
+						facadeModuleId[facadeModuleId.length - 2] || "[name]";
+
+					return `js/${fileName}/[name].[hash].js`;
+				},
+
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name) {
+						const info = assetInfo.name.split(".");
+
+						let extType = info[info.length - 1];
+
+						if (
+							/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(
+								assetInfo.name
+							)
+						) {
+							extType = "media";
+						} else if (
+							/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)
+						) {
+							extType = "img";
+						} else if (
+							/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(
+								assetInfo.name
+							)
+						) {
+							extType = "fonts";
+						}
+
+						return `${extType}/[name]-[hash][extname]`;
+					} else {
+						return `assets/[name]-[hash][extname]`;
+					}
+				}
+			}
+		}
 	}
 });
